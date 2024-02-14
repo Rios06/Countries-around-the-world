43d9a628b05ca382b9b00effd6c5fd3ccb4fad65
davidvalenciaz <mkcallese@gmail.com>
2024-02-14 06:39:21 -0500

    feat(delete): added functionality in the form to remove country from the database

--- src/services/UpdateCountryForm.jsx
+++ src/services/UpdateCountryForm.jsx
@@ -1,29 +1,78 @@
-
+import { useState } from "react";
 
 export const UpdateCountryForm = () => {
+  const [country, setCountry] = useState({});
+  const [inputValue, setValue] = useState("");
+
+  const handleClick = () => {
+    try {
+      fetch(`http://localhost:3000/country/country/${inputValue}`)
+        .then((response) => response.json())
+        .then((data) => setCountry(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClickDelete = () => {
+    try {
+      fetch(`http://localhost:3000/country/country/${inputValue}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((data) => setCountry(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+
   return (
     <>
-     <section className="section-update">
-            <h1>Update <span>Country</span> </h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Sed rerum sapiente cumque ducimus autem in, quos voluptatem
-            </p>
-            <section className="container-update">
-                <input type="text" placeholder="Code"/>
-                <input type="text" placeholder="Name" disabled/>
-                <input type="text" placeholder="Capital" disabled/>
-                <input type="text" placeholder="Continent" disabled/>
-                <input type="text" placeholder="Currency" disabled/>
-                <input type="text" placeholder="Languages" disabled/>
-
-            </section>
-            <div className="button-update">
-            <button>update country</button>
-            <button>delete country</button>
-            </div>
-
+      <section className="section-update">
+        <h1>
+          Update <span>Country</span>{" "}
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed rerum
+          sapiente cumque ducimus autem in, quos voluptatem
+        </p>
+        <section className="container-update">
+          <input
+            type="text"
+            placeholder="Code"
+            onBlur={(e) => setValue(e.target.value)}
+          />
+          <input type="text" placeholder="Name" defaultValue={country.name} />
+          <input
+            type="text"
+            placeholder="Capital"
+            defaultValue={country.capital}
+          />
+          <input
+            type="text"
+            placeholder="Continent"
+            defaultValue={country.continent?.name}
+          />
+          <input
+            type="text"
+            placeholder="Currency"
+            defaultValue={country.currency}
+          />
+          <input
+            type="text"
+            placeholder="Languages"
+            defaultValue={country?.languages?.[0]?.name || ""}
+          />
         </section>
+        <div className="button-update">
+          <button onClick={handleClick}>Consult</button>
+          <button>update country</button>
+          <button onClick={handleClickDelete}>delete country</button>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
